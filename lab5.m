f50e41b7c1db51439bfab18ccb0537decea34072
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-04-03 18:00:38 -0400

    finished lab 5 with fourier transforms

--- lab5.m
+++ lab5.m
@@ -1,36 +1,30 @@
+clc; close all; clear all;
 %% Question 1
-%Format will be f(x) = ..., 0 < x < p (my p was 3)
-%truncated after 1,2..n terms, for me n = 7
+% plot the fourier transforms in the negative direction for all of them,
+% and also make sure to do it for even, odd and identity shift functions. 
+func1 = zeros(1,7); 
+x = linspace(0,2,201); 
 
-a = zeros(1,7); %7 terms so from 1 to 7 for me
+f = sqrt(8+x.^3); 
+x2 = linspace(0,-2,201);
+f2 = sqrt(8+x2.^3); 
+a0 = 2/2*trapz(x,f);
 
-x = linspace(0,2,201); %200 elements for me and from 0 to 3
-%Function
-f = sqrt(8+x.^3); %The one which is given, does NOT change
-
-%Extension
-x2 = linspace(0,-2,201); %This is the extension
-f2 = sqrt(8-x2.^3); %ONLY THIS CHANGES FOR OTHER GRAPHS
-
-
-a0 = 2/2*trapz(x,f); %p = 3 so 2/p
-
-for n = 1:7 %7 curves for me
-    y = f.*cos(n*pi*x/2); %my p was 3 so its divided by 3 
-    a(n) = 2/2*trapz(x,y); %p = 3
-    %Bottom for part C    
+% they asked for 7 curves and my p was 2
+for j = 1:7 
+    y = f.*cos(2*j*pi*x/2);
+    func1(j) = 2/2*trapz(x,y); 
+    y1 = f.*sin (2*j*pi*x/2);
+    b(j) = 2/2*trapz(x,y1);
 end
 
-%Estimate values of fourier series of f
+fh = zeros(7,801);
+x3 = linspace(-4,4,801); 
 
-fh = zeros(7,801); %800 elements for fh and f2h
-x3 = linspace(-4,4,801); % 0 to 3 is 200, 3 to 6 is 400, so 800
-%change -6,6 according to your interval [it's given]
-
-
-for m = 1:7 %7 curves
-for n = 1:m %dont touch this
-    fh(m,:) = fh(m,:) + a(n)*cos(n*pi*x3/2); %p = 3 so x3/3 
+% now we actually need to get the fouriers
+for i = 1:7 
+for j = 1:i 
+    fh(i,:) = fh(i,:) + func1(j)*cos(2*j*pi*x3/2); %p = 3 so x3/3 
 end
 end
 
@@ -38,42 +32,49 @@ fh = a0/2 + fh;
  
 plot(x,f,'r','LineWidth',3);
 hold on;
-plot(x2,f2,'b','LineWidth',3);
+plot(x+2,f,'b','LineWidth',3);
 hold on;
 plot(x3,fh,'k','LineWidth',1);
 
 
-xlim = ([-4 4]);  %Change interval according to yours
+xlim = ([-4 4]);
 xlabel('X');
 ylabel('Y');
 title('Ahmed Fuad Ali, 400075937');
 legend('f','fh','f2','f2h','Location','best');
 
-%----------- end
 
+
+%% Question 2
+theerror = fh(7,501);
+theoreticalanswer = sqrt(9); 
+A = abs(theoreticalanswer-theerror);
 
 
 %% Question 3
 % plot the construction of a discrete fourier transform of the given
 % function 
+
+% first we want to set up our environment and initialize some variables
 clc; close all; clear all;
-
-
 x = linspace(0,2*pi,200);
-
 N = length(x);
+
+% this was my function
 f = 9*sin(15*pi*x) + 2*cos(8*pi*x);
 
+% now we need to use the stuff from the tutorial 
 F = fft(f); 
 
 abs_F = abs(fftshift(F));
 alpha = (0:N-1)-(N/2);
 plot(alpha,abs_F);
 
-%Use log scale
-
+% now plot it
 semilogy(alpha,abs_F);
-
 title('Ahmed Fuad Ali, 400075937');
 xlabel('x');
 ylabel('y');
+
+
+
